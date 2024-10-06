fc1bbdd8a18b121c9cbf12425375ac324d265701
MF <mfregwork21@gmail.com>
2024-10-05 17:15:49 -0700

    PART 1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hey")
+//PART 1 
+let counter = 0
+function incrementCounter() {
+    counter++
+    incrementCounter();
+}
+
+try{
+    incrementCounter()
+} catch(error){
+    console.log("Error: ", error.message)
+    console.log("Last value of counter: ", counter)
+}
